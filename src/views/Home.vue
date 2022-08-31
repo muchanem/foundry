808fbb215938c492dc4878b5196c2473a81f1c10
KJ <kj.schmidt913@gmail.com>
2022-08-31 15:32:25 -0500

    added a lot of styling. still need to style run in colab button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home col-12 mb-5">
     <!-- heading content -->
-    <div class="jumbotron text-center py-5 col-12 bg-light">
-      <h1 class="text-h1 col-10 mx-auto">
+    <v-sheet class="text-center py-10 col-12" color="indigo lighten-2" elevation="12">
+      <h1 class="text-h1 col-10 mx-auto white--text">
         An open source machine learning platform for scientists
       </h1>
-      <p class="text-subtitle-1 mt-3 col-7 mx-auto">
-        Access datasets and ML models while setting industry benchmarks
-      </p>
-    </div>
+      <v-sheet shaped color="red lighten-3" elevation="20" class="col-5 mx-auto">
+        <p class="text-subtitle-1 mt-3 col-12 mx-auto white--text font-weight-medium">
+          Access datasets and ML models while setting industry benchmarks
+        </p>
+      </v-sheet>
+    </v-sheet>
 
     <!-- middle content -->
-    <v-row class="col-md-8 mx-auto col-12">
+    <v-row class="col-md-8 mx-auto col-12 mt-5 mb-8">
       <v-card elevation="3" outlined class="mx-auto col-md-5 col-6 my-6 text-center">
         <a href="https://ai-materials-and-chemistry.gitbook.io/foundry/publishing/publishing-datasets" target="blank">
           <h1 class="display-2"><i class="mdi mdi-chart-bar-stacked red--text text--lighten-3"></i></h1>
@@ -35,9 +37,9 @@
 
 
     <!-- Starter code and metadata section -->
-    <div class="col-10 my-5 mx-auto pt-4">
-      <h1 class="mb-5">We make collaboration easy</h1>
-      <v-row class="mb-5">
+    <v-row class="col-12 px-10 indigo lighten-4 mx-0">
+      <h1 class="col-10 mx-auto">We make collaboration easy</h1>
+      <v-row class="mb-5 col-10 mx-auto">
         <div class="col-6">
           <vue-code-highlight language="python">
             <pre class="language-python">
@@ -46,101 +48,108 @@ f = f.load("foundry_stan_segmentation_v1.1")
 X,y = f.load_data()
 </pre>
           </vue-code-highlight>
-
-          <img alt="dendrite segmentation data snippet" src="../assets/dendrite-seg-img.png" class="col-12 pb-3" />
-
+          <v-sheet rounded color="white" elevation="20">
+            <img alt="dendrite segmentation data snippet" src="../assets/dendrite-seg-img.png" class="col-12 pb-3" />
+          </v-sheet>
         </div>
         <div class="col-5">
-          <p class="fw-light fs-4">
+          <p>
             Once a dataset is uploaded to Foundry, you can load it with just 2
             lines of code.
           </p>
           <a href="https://colab.research.google.com/github/MLMI2-CSSI/foundry/blob/main/examples/dendrite-segmentation/dendrite_segmentation.ipynb"
-            target="blank"><button class="btn btn-primary">
+            target="blank"><v-btn class="">
               Run this in Google Colab
-            </button></a>
+            </v-btn></a>
         </div>
       </v-row>
-      <!-- end of row -->
+    </v-row>
+    <!-- end of row -->
+    <v-row class="col-12 px-10">
       <div class="col-12 text-end">
-        <h1 class="col-11 my-5">and save you valuable time</h1>
+        <h1 class="col-10 mx-auto">and save you valuable time</h1>
       </div>
-      <v-row>
-        <v-col class="col-6">
-          <p class="lead">
+      <v-row class="mb-5 col-10 mx-auto">
+        <v-col class="col-6 mx-auto">
+          <p>
             No need to spend time making sense of someone else's data. We collect information about each dataset to
             take out any guesswork on your part. Our
             publishing process also includes a human review to make sure it meets our
             data standards.
           </p>
         </v-col>
-        <v-col class="col-5">
-          <v-simple-table dense class="col-12 mb-10">
-            <template v-slot:default>
-              <thead>
-                <tr>
-                  <th class="text-left">
-                    Key
-                  </th>
-                  <th class="text-left">
-                    Type
-                  </th>
-                  <th class="text-left">
-                    Units
-                  </th>
-                  <th class="text-left">
-                    Description
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>reference</td>
-                  <td>input</td>
-                  <td></td>
-                  <td>source publication of the band gap value</td>
-                </tr>
-                <tr>
-                  <td>structure</td>
-                  <td>input</td>
-                  <td></td>
-                  <td>the structure of this compound</td>
-                </tr>
-                <tr>
-                  <td>bandgap value (eV)</td>
-                  <td>target</td>
-                  <td>eV</td>
-                  <td>tvalue of the band gap</td>
-                </tr>
-              </tbody>
-            </template>
-          </v-simple-table>
+        <v-col class="col-6 mx-auto">
+          <v-sheet rounded color="white" elevation="20">
+            <v-simple-table dense class="col-12 mb-10">
+              <template v-slot:default>
+                <thead>
+                  <tr>
+                    <th class="text-left">
+                      Key
+                    </th>
+                    <th class="text-left">
+                      Type
+                    </th>
+                    <th class="text-left">
+                      Units
+                    </th>
+                    <th class="text-left">
+                      Description
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>reference</td>
+                    <td>input</td>
+                    <td></td>
+                    <td>source publication of the band gap value</td>
+                  </tr>
+                  <tr>
+                    <td>structure</td>
+                    <td>input</td>
+                    <td></td>
+                    <td>the structure of this compound</td>
+                  </tr>
+                  <tr>
+                    <td>bandgap value (eV)</td>
+                    <td>target</td>
+                    <td>eV</td>
+                    <td>tvalue of the band gap</td>
+                  </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
+          </v-sheet>
         </v-col>
       </v-row>
-      <!-- end of row -->
-    </div>
+    </v-row>
+    <!-- end of row -->
+
+    <v-divider></v-divider>
 
     <!-- be a part of foundry section -->
 
-
-    <div class="col-10 mx-auto mt-5">
-      <h1 class="text-center my-3 text-h1">Build Foundry With Us</h1>
-      <p class="col-8 mx-auto">
-        At Foundry, we believe accessibility and collaboration are the keys to
-        problem solving. That's why we've made it super easy for you to share
-        and use data. If you have a dataset you'd like to contribute, our
-        <a href="https://github.com/MLMI2-CSSI/foundry/tree/main/examples/publishing-guides" target="blank">publishing
-          guide</a>
-        can set you up to do that.
-      </p>
-      <p class="col-8 mx-auto">
-        Check out our
-        <a href="https://ai-materials-and-chemistry.gitbook.io/foundry/v/docs/" target="blank">documentation</a>
-        to see how easy it is to use Foundry, and follow our
-        <a href="https://github.com/MLMI2-CSSI/foundry/tree/main/examples" target="blank">examples</a>
-        to do it all yourself.
-      </p>
-    </div>
+    <v-row class="indigo lighten-4 pt-3 pb-10">
+      <v-sheet rounded color="red lighten-3" elevation="5" class="col-8 mx-auto mt-5">
+        <h1 class="text-center my-3 text-h2 white--text font-weight-regular">Build Foundry With Us</h1>
+        <p class="col-11 mx-auto white--text font-weight-medium">
+          At Foundry, we believe accessibility and collaboration are the keys to
+          problem solving. That's why we've made it super easy for you to share
+          and use data. If you have a dataset you'd like to contribute, our
+          <a href="https://github.com/MLMI2-CSSI/foundry/tree/main/examples/publishing-guides" target="blank">publishing
+            guide</a>
+          can set you up to do that.
+        </p>
+        <p class="col-11 mx-auto white--text font-weight-medium">
+          Check out our
+          <a href="https://ai-materials-and-chemistry.gitbook.io/foundry/v/docs/" target="blank">documentation</a>
+          to see how easy it is to use Foundry, and follow our
+          <a href="https://github.com/MLMI2-CSSI/foundry/tree/main/examples" target="blank">examples</a>
+          to do it all yourself.
+        </p>
+      </v-sheet>
+    </v-row>
 
   </div>
 </template>
