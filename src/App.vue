5e9035cc2ea49cfd5076188e1f2b120814f92195
KJ <kj.schmidt913@gmail.com>
2022-09-02 14:16:53 -0400

    responsive nav bar complete

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,22 @@
 <template>
   <v-app id="inspire">
-
-    <!-- desktop nav bar -->
-    <!-- <v-toolbar class="flex-grow-0 d-none d-md-flex">
-
-      <a href="/">
-        <v-toolbar-title>
-          <img alt="Foundry logo icon" src="./assets/logo-full.png" style="width: 200px" />
-        </v-toolbar-title>
-      </a>
-
-      <v-spacer class="d-none d-md-flex"></v-spacer>
-
-      <v-toolbar-items class="d-none d-md-flex" v-for="item in items" :key="item.title" :to="item.to" link>
-        <a :href="item.to" class="text-decoration-none indigo--text darken-1 ">
-          <v-toolbar-title class="mx-4 mt-4 text-body-1">{{ item.title }}</v-toolbar-title>
-        </a>
-      </v-toolbar-items>
-    </v-toolbar> -->
-    <!-- end of desktop nav bar -->
-
-    <!-- start of mobile nav bar -->
-    <div class="d-xl-none d-lg-none d-md-none">
-      <v-navigation-drawer disable-resize-watcher v-model="drawer" app null class="d-xl-none d-lg-none d-md-none">
-        <img alt="Foundry logo icon" src="./assets/logo-full.png" style="width: 200px" />
-
-
-        <v-divider></v-divider>
-
-        <v-list dense nav>
-          <v-list-item v-for="item in items" :key="item.title">
-            <a :href="item.to" class="text-decoration-none">
-              <v-list-item-icon class="d-inline">
-                <v-icon class="d-inline">{{ item.icon }}</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content class="d-inline">
-                <v-list-item-title class="d-inline">{{ item.title }}</v-list-item-title>
-              </v-list-item-content>
-            </a>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-    </div>
+    <!-- mobile nav -->
+    <v-navigation-drawer disable-resize-watcher v-model="drawer" app null class="d-xl-none d-lg-none d-md-none">
+      <img alt="Foundry logo icon" src="./assets/logo-full.png" style="width: 200px" />
+      <v-divider></v-divider>
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title">
+          <a :href="item.to" class="text-decoration-none grey--text text--darken-2">
+            <v-list-item-icon class="d-inline">
+              <v-icon class="d-inline" color="indigo lighten-1 ">{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content class="d-inline">
+              <v-list-item-title class="d-inline">{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </a>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-app-bar app color="white">
       <v-app-bar-nav-icon class="d-md-none d-lg-none d-xl-none" @click="drawer = !drawer"></v-app-bar-nav-icon>
@@ -54,17 +29,14 @@
 
       <v-spacer class="d-none d-md-flex"></v-spacer>
 
-      <v-toolbar-items class="d-none d-md-flex text-decoration-none indigo--text darken-1 " v-for="item in items"
+      <v-toolbar-items class="d-none d-md-flex" v-for="item in items"
         :key="item.title" :to="item.to" link>
-        <a :href="item.to" class="text-decoration-none indigo--text darken-1 ">
+        <a :href="item.to" class="text-decoration-none indigo--text text--darken-1 ">
           <v-toolbar-title class="mx-4 mt-4 text-body-1">{{ item.title }}</v-toolbar-title>
         </a>
       </v-toolbar-items>
     </v-app-bar>
 
-
-
-    <!-- end of mobile nav bar -->
 
     <v-main>
       <router-view></router-view>
