dd99f51aeb6e20574c9f086d81b575e4a5c6fea8
KJ <kj.schmidt913@gmail.com>
2022-09-02 14:06:23 -0400

    responsive nav behavior working

--- src/App.vue
+++ src/App.vue
@@ -21,25 +21,27 @@
     <!-- end of desktop nav bar -->
 
     <!-- start of mobile nav bar -->
-    <v-navigation-drawer v-model="drawer" app null class="d-none-xl d-none-lg">
-      <img alt="Foundry logo icon" src="./assets/logo-full.png" style="width: 200px" />
+    <div class="d-xl-none d-lg-none d-md-none">
+      <v-navigation-drawer disable-resize-watcher v-model="drawer" app null class="d-xl-none d-lg-none d-md-none">
+        <img alt="Foundry logo icon" src="./assets/logo-full.png" style="width: 200px" />
 
-  
-      <v-divider></v-divider>
 
-      <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title">
-          <a :href="item.to" class="text-decoration-none">
-          <v-list-item-icon class="d-inline">
-            <v-icon class="d-inline">{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content class="d-inline">
-            <v-list-item-title class="d-inline">{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-          </a>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+        <v-divider></v-divider>
+
+        <v-list dense nav>
+          <v-list-item v-for="item in items" :key="item.title">
+            <a :href="item.to" class="text-decoration-none">
+              <v-list-item-icon class="d-inline">
+                <v-icon class="d-inline">{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content class="d-inline">
+                <v-list-item-title class="d-inline">{{ item.title }}</v-list-item-title>
+              </v-list-item-content>
+            </a>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </div>
 
     <v-app-bar app color="white">
       <v-app-bar-nav-icon class="d-md-none d-lg-none d-xl-none" @click="drawer = !drawer"></v-app-bar-nav-icon>
@@ -52,7 +54,8 @@
 
       <v-spacer class="d-none d-md-flex"></v-spacer>
 
-      <v-toolbar-items class="d-none d-md-flex text-decoration-none indigo--text darken-1 " v-for="item in items" :key="item.title" :to="item.to" link>
+      <v-toolbar-items class="d-none d-md-flex text-decoration-none indigo--text darken-1 " v-for="item in items"
+        :key="item.title" :to="item.to" link>
         <a :href="item.to" class="text-decoration-none indigo--text darken-1 ">
           <v-toolbar-title class="mx-4 mt-4 text-body-1">{{ item.title }}</v-toolbar-title>
         </a>
@@ -72,7 +75,7 @@
 <script>
 export default {
   data: () => ({
-    drawer: null,
+    drawer: false,
     items: [
       { title: 'Home', icon: 'mdi-home', to: '/' },
       { title: 'Datasets', icon: 'mdi-magnify', to: '#/datasets' },
