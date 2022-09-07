9912d6862f6bb56821891c536a14bc2fca69b741
KJ <kj.schmidt913@gmail.com>
2022-09-07 08:17:17 -0400

    added links to all logos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -166,10 +166,11 @@ X,y = f.load_data()
     <v-footer padless color="white" class="col-12 mx-0 px-0 mt-10">
       <v-sheet color="indigo lighten-2" elevation="12" class="mx-auto text-center mt-10 pb-3 pt-10 col-12">
         <v-sheet rounded="xl" color="white" class="col-md-9 col-11 mx-auto">
-          <img alt="logo" src="../assets/logo_UC.png" class="px-8" />
-          <img alt="logo" src="../assets/logo_UW.png" class="px-8" />
-          <img alt="logo" src="../assets/logo_NSF.png" class="px-8" />
-          <img alt="logo" src="../assets/logo_ANL.png" class="px-8" />
+          <a target="blank" href="https://www.uchicago.edu/"><img alt="logo" src="../assets/logo_UC.png"
+              class="px-8" /></a>
+          <a target="blank" href="https://www.wisc.edu/"><img alt="logo" src="../assets/logo_UW.png" class="px-8" /></a>
+          <a target="blank" href="https://www.nsf.gov/"><img alt="logo" src="../assets/logo_NSF.png" class="px-8" /></a>
+          <a target="blank" href="https://www.anl.gov/"><img alt="logo" src="../assets/logo_ANL.png" class="px-8" /></a>
         </v-sheet>
         <p class="pt-5 col-md-8 col-11 mx-auto font-weight-light white--text">This work was supported by the <a
             class="red--text text--lighten-4" target="blank" href="https://www.nsf.gov/">National
@@ -185,8 +186,10 @@ X,y = f.load_data()
 
 
         <div class="mx-auto">
-          <img alt="logo" src="../assets/logo_MDF.png" class="px-6" />
-          <img alt="logo" src="../assets/logo_globus.png" class="px-6" />
+          <a href="https://materialsdatafacility.org/" target="blank"><img alt="logo" src="../assets/logo_MDF.png"
+              class="px-6" /></a>
+          <a href="https://globus.org/" target="blank"><img alt="logo" src="../assets/logo_globus.png"
+              class="px-6" /></a>
         </div>
         <v-card-text class="white--text pt-3  mx-auto text-body-1">
           <v-icon color="white" class="pb-3">mdi-arm-flex</v-icon> Powered by <a
