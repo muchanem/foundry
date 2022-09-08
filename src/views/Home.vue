09f8bd6f88845ab28f1429efc4e9e77136fc34e7
Ben Blaiszik <blaiszik@uchicago.edu>
2022-09-08 08:48:26 -0500

    Adding static header for the moment...

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -180,24 +180,6 @@ export default {
   name: 'Home',
   components: {
     VueCodeHighlight
-  },
-  metaInfo: {
-    meta: [
-      // Twitter Card
-      { name: 'twitter:card', content: 'summary' },
-      { name: 'twitter:site', content:'@BenBlaiszik'},
-      { name: 'twitter:title', content: 'Foundry' },
-      { name: 'twitter:description', content: 'ML-ready datasets for materials science and chemistry' },
-      // image must be an absolute path
-      { name: 'twitter:image', content: 'https://foundry-ml.org/img/logo-full.ec32661a.png' },
-
-      // Facebook OpenGraph
-      { property: 'og:type', content: 'article' },
-      { property: 'og:title', content: 'Foundry' },
-      { property: 'og:site_name', content: 'Vue Example' },
-      { property: 'og:image', content: 'https://foundry-ml.org/img/logo-full.ec32661a.png' },
-      { property: 'og:description', content: 'ML-ready datasets for materials science and chemistry' }
-    ]
   }
 }
 </script>
